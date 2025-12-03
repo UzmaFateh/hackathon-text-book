f6cbdefc902d9430f01bcb6d1f0fa4394d172fe3
UzmaFateh <uzmafateh901@gmail.com>
2025-12-03 14:41:34 +0500

    rechecking

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -16,10 +16,13 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://your-docusaurus-site.example.com',
-  url: 'https://hackathon-text-book.vercel.app',
+  // url: 'https://hackathon-text-book.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // baseUrl: '/',
+  url: 'https://hackathon-text-book.vercel.app',
   baseUrl: '/',
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
